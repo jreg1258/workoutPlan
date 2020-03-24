ca2267367b10118c20dcc656a51399bfa0c3190f
Joey Register <josephregister1258@gmail.com>
2020-03-24 10:09:57 -0400

    edits to api route (get)

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -14,8 +14,14 @@ module.exports = function(app) {
 
   // route to post a new workout
   app.post("/api/workouts", ({ body }, res) => {
+<<<<<<< HEAD
     Workout.create(body,{$push: {exercises: body}})
     console.log(body)
+=======
+    if (Workout.findOne({day: { $gte: Date.now() }})) {
+    Workout.findOneAndUpdate({day: { $gte: Date.now() }}, {$push: {exercise: body}})
+    }
+>>>>>>> parent of d4e1c29... fixed routes
     Workout.create(body)
       .then(results => {
         res.json(results);
@@ -28,17 +34,23 @@ module.exports = function(app) {
   // route to update a workout
   app.put("/api/workouts/:id", ({ params, body }, res) => {
     // find the id to update the database 
+<<<<<<< HEAD
     console.log(body)
     Workout.findByIdAndUpdate({ _id : params.id }, 
       {$push: {exercises: [{body
       }]}})
+=======
+    Workout.findByIdAndUpdate({ _id : params.id }, body)
+>>>>>>> parent of d4e1c29... fixed routes
       .then(()=>{
-        Workout.findByIdAndUpdate({ _id : params.id }, {$inc:{tDuration: body.duration}}
+        console.log(body)
+        Workout.findByIdAndUpdate({ _id : params.id }, {$inc:{totalDuration: body.duration}}
         )})
           .then(() => {
         // have to do another .then to return a promise that updates the front-end
             Workout.findOne({ _id: params.id })
               .then(results => {
+                console.log(results)
                 res.json(results)
         })  
       })
